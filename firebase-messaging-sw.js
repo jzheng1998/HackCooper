f44e734ebf07cfc80765463d21e57f34c1149d8f
cyong813 <cy986@nyu.edu>
2018-10-13 00:09:23 -0400

    completed basic msg app

---
+++ firebase-messaging-sw.js
@@ -0,0 +1,7 @@
+// Service Worker that receives and displays web notifications
+
+importScripts('/__/firebase/5.1.0/firebase-app.js');
+importScripts('/__/firebase/5.1.0/firebase-messaging.js');
+importScripts('/__/firebase/init.js');
+
+firebase.messaging();
